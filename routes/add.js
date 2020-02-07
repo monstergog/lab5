b35d19b58596318f2d14b3062e655b9cf9efc1f9
Alex <aminhn95@gmail.com>
2020-02-06 16:33:03 -0800

    lab 5 finished

--- routes/add.js
+++ routes/add.js
@@ -1,15 +1,13 @@
 var data = require("../data.json");
 
 exports.addFriend = function(request, response){   
+	data.friends.push({
+		"name": request.query.name,
+		"description": request.query.description,
+		"imageURL": "http://lorempixel.com/400/400/people"
+		});
 	
-	//"name": 
-	request.query.name;
-	//"description": 
-	request.query.description;
-	//"imageURL": "http://uploads.edubilla.com/awards-winners/92/b4/allen-newell.jpg"
-	
-	//data.friend.push(newFriend);
-	
-	console.log(data);
+	console.log(request.query.name);
+	console.log(request.query.description);
 	response.render('index', data);
 };
